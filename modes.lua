629b8f3b253248a890866900b91f35acc5844c2d
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-05-29 15:27:34 -0700

    add enableWindow flag

--- modes.lua
+++ modes.lua
@@ -3,6 +3,8 @@ local imgui = require('imgui')
 local helpers = gFunc.LoadFile('smart.lac/helpers.lua')
 
 modeTable = {}
+modeTable.enableWindow = true
+
 
 modeTable.modes = {}
 modeTable.activeMode = ''
@@ -27,26 +29,34 @@ return {
   end,
   initializeWindow = function()
     ashita.events.register('d3d_present', 'present_cb', function()
-      local flags = bit.bor(
-        ImGuiWindowFlags_NoDecoration,
-        ImGuiWindowFlags_AlwaysAutoResize,
-        ImGuiWindowFlags_NoSavedSettings,
-          ImGuiWindowFlags_NoFocusOnAppearing,
-          ImGuiWindowFlags_NoNav
-      )
-      imgui.SetNextWindowBgAlpha(0.8)
-      imgui.SetNextWindowSize({200, -1}, ImGuiCond_Always)
-      imgui.SetNextWindowSizeConstraints({-1, -1}, {FLT_MAX, FLT_MAX})
-      imgui.SetNextWindowPos({1400, 400}, ImGuiCond_Always, {0, 0})
-      if (imgui.Begin('smart.lac', true, flags)) then
-        imgui.SetWindowFontScale(1)
-        imgui.Text("Smart.LAC")
-        imgui.Separator()
-        imgui.Text('Current Mode: ')
-        imgui.SameLine()
-        imgui.Text(modeTable.activeMode or 'None')
-        imgui.End()
+      if modeTable.enableWindow then
+        local flags = bit.bor(
+          ImGuiWindowFlags_NoDecoration,
+          ImGuiWindowFlags_AlwaysAutoResize,
+          ImGuiWindowFlags_NoSavedSettings,
+            ImGuiWindowFlags_NoFocusOnAppearing,
+            ImGuiWindowFlags_NoNav
+        )
+        imgui.SetNextWindowBgAlpha(0.8)
+        imgui.SetNextWindowSize({200, -1}, ImGuiCond_Always)
+        imgui.SetNextWindowSizeConstraints({-1, -1}, {FLT_MAX, FLT_MAX})
+        imgui.SetNextWindowPos({1400, 400}, ImGuiCond_Always, {0, 0})
+        if (imgui.Begin('smart.lac', true, flags)) then
+          imgui.SetWindowFontScale(1)
+          imgui.Text("Smart.LAC")
+          imgui.Separator()
+          imgui.Text('Current Mode: ')
+          imgui.SameLine()
+          imgui.Text(modeTable.activeMode or 'None')
+          imgui.End()
+        end
       end
     end)
+  end,
+  setWindowVisibility = function(b)
+    modeTable.enableWindow = b
+  end,
+  toggleWindowVisibility = function()
+    modeTable.enableWindow = not modeTable.enableWindow
   end
 }
