d139b47aca547da28fd2d9ca3eee3dd374c89471
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-01-25 14:47:09 -0800

    fix bug in BRD handler

--- handlers/JOB/BRD.lua
+++ handlers/JOB/BRD.lua
@@ -3,7 +3,7 @@ return {
     midcast = function(action, sets)
         if action.Type == "Bard Song" then
             if sets.settings ~= nil and sets.settings.DummySongs ~= nil then
-                if sets.settings.DummySongs:contains(action.Name) then
+                if T(sets.settings.DummySongs):contains(action.Name) then
                     return sets.midcast.dummySongs ~= nil and sets.midcast.dummySongs or false
                 end
             end
