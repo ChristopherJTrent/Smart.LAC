f8e607d3873c3976814ba56794c0a0fdffe2838b
Christopher Trent <ChristopherJTrent@outlook.com>
2025-01-27 21:50:33 -0800

    add failsafe if invalid data is passed to setWindowLocation

--- smart.lua
+++ smart.lua
@@ -148,9 +148,9 @@ local command = function(args)
 			if #args ~= 3 then
 				print(helpers.AddModHeader("setWindowLocation requires exactly 3 arguments"))
 			elseif string.lower(args[2]) == 'x' then
-				modes.setWindowPosX(tonumber(args[3]))
+				modes.setWindowPosX(tonumber(args[3]) or ModeTable.imgui.windowPosX)
 			elseif string.lower(args[2]) == 'y' then
-				modes.setWindowPosY(tonumber(args[3]))
+				modes.setWindowPosY(tonumber(args[3]) or ModeTable.imgui.windowPosY)
 			else 
 				print(helpers.AddModHeader("second argument must be either x or y"))
 			end
