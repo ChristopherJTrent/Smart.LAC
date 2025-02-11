d96140e90618543813220040235d44f92bf21f54
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-11 10:52:15 -0800

    implement gFunc.LoadFile from LuAshitacast SDK

--- test/common/gFunc.lua
+++ test/common/gFunc.lua
@@ -1,5 +1,15 @@
 gFunc = {}
 
+local function fileExists(path)
+	local f = io.open(path, 'r')
+	if f~= nil then
+		io.close(f)
+		return true
+	else 
+		return false
+	end
+end
+
 gFunc.Combine = function(base, override)
 	local newSet = {}
 
@@ -14,4 +24,49 @@ gFunc.Combine = function(base, override)
 		end
 	end
 	return newSet
+end
+
+gFunc.LoadFile = function(path)
+	local sep = package.config:sub(1, 1)
+	local frameworkRoot = string.gsub(arg[0], string.format("%stests.lua", sep), "")
+	local paths = T{
+		path,
+		string.format('%s.lua', path),
+		string.format("%s%stest%sshared%s%s", frameworkRoot, sep, sep, sep, path),
+		string.format("%s%stest%sshared%s%s.lua", frameworkRoot, sep, sep, sep, path),
+		string.format("%s%s%s", frameworkRoot, sep, path),
+		string.format("%s%s%s.lua", frameworkRoot, sep, path),
+	}
+	for token in string.gmatch(package.path, "[^;]+") do
+        paths:append(string.gsub(token, '?', path));
+    end
+
+	local filePath
+	for _, path in ipairs(paths) do
+		if fileExists(path) then
+			filePath = path
+			break
+		end
+	end
+
+	if filePath == nil then
+		error("Attempted to load file that does not exist.")
+	end
+	
+	local func, loadError = loadfile(filePath)
+	if not func then
+		error('Attempted to load a file that isn\'t lua code')
+	end
+
+	local fileValue = nil
+	local success, execError = pcall(function()
+		fileValue = func()
+	end)
+
+	if not success then
+		print(execError)
+		error('failed to execute file.')
+	end
+
+	return fileValue
 end
