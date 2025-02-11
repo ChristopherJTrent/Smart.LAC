c1a3bffa60e7f9f1567cf98a0972d088260c34c6
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-10 21:03:49 -0800

    write geomaner tests

---
+++ test/JobHandlers/GEO.lua
@@ -0,0 +1,35 @@
+---@type jobHandler
+local geomancer = require('handlers.JOB.GEO')
+local lu = require('luaunit')
+
+TestGeomancer = T{
+	sets = T{
+		midcast = T{
+			Geomancy = T{
+				Head = '1'
+			},
+			geocolure = T{
+				Body = '1'
+			},
+			indicolure = T{
+				Hands = '1'
+			}
+		}
+	}
+}
+
+function TestGeomancer:testMidcast()
+	lu.assertEquals(geomancer.midcast({}, {}), false)
+end
+
+function TestGeomancer:testGeocolure()
+	lu.assertEquals(geomancer.midcast({
+		Name = "Geo-Frailty"
+	}, self.sets), {Body = '1'})
+end
+
+function TestGeomancer:testIndicolure()
+	lu.assertEquals(geomancer.midcast({
+		Name = "Indi-Fury",
+	}, self.sets), {Hands = '1'})
+end
