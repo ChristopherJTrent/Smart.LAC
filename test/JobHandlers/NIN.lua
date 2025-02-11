b508983b3aa9abf98c35c6941b358c9b025cb9b7
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-10 21:03:54 -0800

    write ninja tests

---
+++ test/JobHandlers/NIN.lua
@@ -0,0 +1,23 @@
+---@type jobHandler
+local ninja = require('handlers.JOB.NIN')
+local lu = require('luaunit')
+
+TestNinja = {
+	sets = T{
+		precast = T{
+			utsusemi = T{
+				Head = '1'
+			}
+		}
+	}
+}
+
+function TestNinja:testPrecastMissing()
+	lu.assertEquals(ninja.precast({}, {}), false)
+end
+
+function TestNinja:testUtsusemiPrecast()
+	lu.assertEquals(ninja.precast({
+		Name = "Utsusemi: Ichi"
+	}, self.sets), { Head = '1' })
+end
