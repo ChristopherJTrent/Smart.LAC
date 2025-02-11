0fef2a426b78b9d4f6f8df4494de26de407dfc8f
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-11 13:34:18 -0800

    write additional helpers tests

--- test/core/helpers.lua
+++ test/core/helpers.lua
@@ -18,3 +18,71 @@ function TestHelpers:testContainsAllKeys()
 	lu.assertTrue(helpers.ContainsAllKeys({a = 1}, {a = 1, b = 2}))
 	lu.assertFalse(helpers.ContainsAllKeys({a = 1, b = 2}, {a = 1}))
 end
+
+function TestHelpers:testValidatePlayerData()
+	lu.assertNotNil(helpers.ValidatePlayerData)
+	lu.assertTrue(helpers.ValidatePlayerData({ownedBelts = {['Fotia Belt'] = true}, ownedGorgets = {['Fotia Gorget'] = true}, DisableWeaponWarning = false}))
+end
+
+function TestHelpers:testCustomFlattenTable()
+	lu.assertNotNil(helpers.customFlattenTable)
+	lu.assertEquals(helpers.customFlattenTable({}), {})
+	lu.assertEquals(helpers.customFlattenTable({Head = "test"}), { "test" })
+	lu.assertEquals(helpers.customFlattenTable({
+		midcast = {
+			Head = "test"
+		}
+	}), { "test" })
+	lu.assertEquals(helpers.customFlattenTable({
+		midcast = {
+			Head = {
+				Name = "test",
+				Augment = "missing"
+			}
+		}
+	}), { "test" })
+end
+
+function TestHelpers:testValidateSets()
+	lu.assertNotNil(helpers.ValidateSets)
+	lu.assertFalse(helpers.ValidateSets({}))
+	lu.assertTrue(helpers.ValidateSets({
+		weaponskill = T{
+			default = T{}
+		},
+		general = T{
+			Idle = T{},
+			Engaged = T{}
+		},
+		ability = T{
+			default = T{}
+		},
+		item = T{
+			default = T{}
+		},
+		settings = T{}
+	}))
+end
+
+function TestHelpers:testAddModHeader()
+	lu.assertNotNil(helpers.AddModHeader)
+	lu.assertEquals(helpers.AddModHeader(""), "[LG][Smart.LAC] [RS]")
+	lu.assertEquals(helpers.AddModHeader("test"), "[LG][Smart.LAC] [RS]test")
+end
+
+function TestHelpers:testSucceedOrWarn()
+	lu.assertNotNil(helpers.SucceedOrWarn)
+	lu.assertEquals(helpers.SucceedOrWarn(true, "test", "fail"), "successtest")
+	lu.assertEquals(helpers.SucceedOrWarn(false, "test", "fail"), "warningfail")
+end
+
+function TestHelpers:testSucceedOrError()
+	lu.assertNotNil(helpers.SucceedOrError)
+	lu.assertEquals(helpers.SucceedOrError(true, "test", "fail"), "successtest")
+	lu.assertEquals(helpers.SucceedOrError(false, "test", "fail"), "errorfail")
+end
+
+function TestHelpers:testGetWeaponskillProperties()
+	lu.assertNotNil(helpers.GetWeaponskillProperty)
+	lu.assertEquals(helpers.GetWeaponskillProperty({Id = 1}), {en="Combo", skillchain= {'Impaction'}})
+end
