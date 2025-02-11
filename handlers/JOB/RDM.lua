cb2442b03d865e5d29b4683bc7b55b6c4b5ff265
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-11 10:15:07 -0800

    Fix errors in RDM handler

--- handlers/JOB/RDM.lua
+++ handlers/JOB/RDM.lua
@@ -3,22 +3,23 @@ return {
         if sets.midcast == nil then return false end
         if action.Skill == "Enfeebling Magic" then
             if sets.midcast['Enfeebling Magic'] == nil then return false end
-            local finalSet = sets.midcast['Enfeebling Magic'].default or {}
+            local finalSet = sets.midcast['Enfeebling Magic'].default ~= nil 
+                         and sets.midcast['Enfeebling Magic'].default
+                         or  {}
             local groups = T{
-                highSkill = T{"Distract III", "Frazzle III"},
-                lowSkill = T{"Poison", "Poison II"},
-                skill = T{"Distract III", "Frazzle III", "Poison", "Poison II"},
-                duration = T{"Inundation"},
-                mAccDuration = T{"Sleep", "Sleep II", "Bind", "Break", "Silence"},
-                mAccPotency = T{"Gravity", "Gravity II"},
-                mAcc = T{"Frazzle", "Frazzle II", "Dispel", "Distract", "Distract II"},
-                MND = T{"Paralyze", "Paralyze II", "Addle", "Addle II", "Slow", "Slow II"},
-                INT = T{"Blind", "Blind II"}
+                {Name = "highSkill", Spells = T{"Distract III", "Frazzle III"} },
+                {Name = "lowSkill", Spells = T{"Poison", "Poison II"} },
+                {Name = "skill", Spells = T{"Distract III", "Frazzle III", "Poison", "Poison II"}},
+                {Name = "duration", Spells = T{"Inundation"}},
+                {Name = "mAccDuration", Spells = T{"Sleep", "Sleep II", "Bind", "Break", "Silence"}},
+                {Name = "mAccPotency", Spells = T{"Gravity", "Gravity II"}},
+                {Name = "mAcc", Spells = T{"Frazzle", "Frazzle II", "Dispel", "Distract", "Distract II"}},
+                {Name = "MND", Spells = T{"Paralyze", "Paralyze II", "Addle", "Addle II", "Slow", "Slow II"}},
+                {Name = "INT", Spells = T{"Blind", "Blind II"}}
             }
-
-            for group, spells in ipairs(groups) do
-                if sets.midcast['Enfeebling Magic'][group] ~= nil and spells.include(action.Name) then
-                    return gFunc.Combine(finalSet, sets.midcast['Enfeebling Magic'][group])
+            for _, group in ipairs(groups) do
+                if sets.midcast['Enfeebling Magic'][group.Name] ~= nil and T(group.Spells):hasval(action.Name) then
+                    return gFunc.Combine(finalSet, sets.midcast['Enfeebling Magic'][group.Name])
                 end
             end
             if sets.midcast['Enfeebling Magic'].default ~= nil then
@@ -29,26 +30,27 @@ return {
         elseif action.Skill == "Enhancing Magic" then
             if sets.midcast['Enhancing Magic'] ~= nil then 
                 local groups = T{
-                    skill = T{"Aquaveil", "Stoneskin", "Temper", "Temper II"},
-                    duration = T{"Blink", "Haste", "Haste II", "Flurry", "Flurry II"} -- check for barspells
+                    {Name = "skill", Spells = T{"Aquaveil", "Stoneskin", "Temper", "Temper II"}},
+                    {Name = "duration", Spells = T{"Blink", "Haste", "Haste II", "Flurry", "Flurry II"}} -- check for barspells
                 }
                 
-                local finalSet = sets.midcast['Enhancing Magic'].default or {}
-                for group, spells in ipairs(groups) do
-                    if sets.midcast['Enhancing Magic'][group] ~= nil and spells.include(action.Name) then
-                        return gFunc.Combine(finalSet, sets.midcast['Enhancing Magic'][group])
+                local finalSet = sets.midcast['Enhancing Magic'].default 
+                             and sets.midcast['Enhancing Magic'].default
+                             or {}
+                for _, group in ipairs(groups) do
+                    if sets.midcast['Enhancing Magic'][group.Name] ~= nil and group.Spells:hasval(action.Name) then
+                        return gFunc.Combine(finalSet, sets.midcast['Enhancing Magic'][group.Name])
                     end
                 end
-                if string.find(action.Name, "bar") ~= nil then
+                if string.find(action.Name, "Bar") ~= nil then
                     return gFunc.Combine(finalSet, sets.midcast['Enhancing Magic'].duration)
                 end
-                if sets.midcast.skill ~= nil and string.find(action.Name, "^En.*") ~= nil then
-                    return gFunc.Combine(finalSet, sets.midcast.skill)
+                if sets.midcast['Enhancing Magic'].skill ~= nil and string.find(action.Name, "En") ~= nil then
+                    return gFunc.Combine(finalSet, sets.midcast['Enhancing Magic'].skill)
                 end
                 if string.find(action.Name, "Gain") ~= nil then
                     return gFunc.Combine(
-                        finalSet, 
-                        sets.midcast['Enhancing Magic'].skill ~= nil and sets.midcast['Enhancing Magic'].skill or {}, 
+                        gFunc.Combine(finalSet, sets.midcast['Enhancing Magic'].skill ~= nil and sets.midcast['Enhancing Magic'].skill or {}), 
                         sets.midcast['Enhancing Magic'].gainspell ~= nil and sets.midcast['Enhancing Magic'].gainspell or {}
                     )
                 end
