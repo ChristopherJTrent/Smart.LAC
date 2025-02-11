62995ac6cc5a335e5a929bdde9c81eb4bcfc2d40
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-11 13:34:12 -0800

    add chat mocks

--- test/common/ashita.lua
+++ test/common/ashita.lua
@@ -1,3 +1,18 @@
 ashita = {}
 ashita.events = {}
 ashita.events.register = function(a, b, c) end
+chat = T{
+	colors = T{
+		LawnGreen = "[LG]",
+		Reset = "[RS]"
+	},
+	success = function(string)
+		return "success"..string
+	end,
+	warning = function(string)
+		return "warning"..string
+	end,
+	error = function(string)
+		return "error"..string
+	end
+}
