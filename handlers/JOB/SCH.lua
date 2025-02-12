647f9c5c61ea27a45444464ee11cad27b9ea7a6c
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-11 17:46:47 -0800

    fix issue in sch handler

--- handlers/JOB/SCH.lua
+++ handlers/JOB/SCH.lua
@@ -2,11 +2,11 @@ return {
 	midcast = function(action, sets)
 		if sets.midcast == nil then return false end
 		---@type Action
-		if action.Name:contains('helix') then
+		if string.find(action.Name, 'helix') ~= nil then
 			if sets.midcast.helix then
 				return sets.midcast.helix
 			end
-		elseif action.Name:contains('storm') then
+		elseif string.find(action.Name, 'storm') then
 			if sets.midcast.storm then return sets.midcast.storm end
 		end
 	end
