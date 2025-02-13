20ef16f9e6f6cb6a89005c9b3af82f2e04556ab1
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-12 23:31:18 -0800

    add belts and gorgets to shared data

--- test/shared/index.lua
+++ test/shared/index.lua
@@ -1,9 +1,27 @@
 return {
+	---@type table<string, boolean>
 	ownedBelts = T{
-
+		['Flame Belt']   = true,
+		['Soil Belt']    = true,
+		['Aqua Belt']    = true,
+		['Breeze Belt']  = true,
+		['Snow Belt']    = true,
+		['Thunder Belt'] = true,
+		['Light Belt']   = true,
+		['Shadow Belt']  = true,
+		['Fotia Belt']   = true
 	},
+	---@type table<string, boolean>
 	ownedGorgets = T{
-
+		['Flame Gorget']   = true,
+		['Soil Gorget']    = true,
+		['Aqua Gorget']    = true,
+		['Breeze Gorget']  = true,
+		['Snow Gorget']    = true,
+		['Thunder Gorget'] = true,
+		['Light Gorget']   = true,
+		['Shadow Gorget']  = true,
+		['Fotia Gorget']   = true
 	},
 	posX = 0,
 	posY = 0,
