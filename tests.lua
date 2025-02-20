42041e4814ba78c4af0a25026c321e836f30fc57
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-02-20 12:15:28 -0800

    initialize modes before running tests

--- tests.lua
+++ tests.lua
@@ -5,6 +5,7 @@ local lu = require('luaunit')
 require('luacov')
 ---@diagnostic disable-next-line: different-requires
 require('test.common')
+require('smart')
 
 require('test.jobHandlers')
 require('test.core')
