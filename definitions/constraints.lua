64a7b6d83f81f2684a1e9a8f97b6de459ae18708
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-03-29 01:46:12 -0700

    tweak logic for dual-wield constraint

--- definitions/constraints.lua
+++ definitions/constraints.lua
@@ -4,7 +4,15 @@ Constraint = {
 }
 
 function Constraint.HasDualWield()
-	return AshitaCore:GetMemoryManager():GetPlayer():HasAbility(DualWieldAbilityID)
+    ---@type Player
+    local player = gData.GetPlayer()
+    return (
+        ( player.MainJob == "NIN" and player.MainJobSync > 10 )
+        or ( player.SubJob == "NIN" and player.SubJobSync > 10 )
+        or ( player.MainJob == "DNC" and player.MainJobSync > 20 )
+        or ( player.SubJob == "DNC" and player.SubJobSync > 20 )
+        or AshitaCore:GetMemoryManager():GetPlayer():HasAbility(DualWieldAbilityID)
+    )    
 end
 
 function Constraint.Invert(constraint)
