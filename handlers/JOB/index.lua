b3a6ae4e31efe60eddc644dd22e80374b9562fce
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-04-04 13:38:25 -0700

    add job-specific handlers for RUN

--- handlers/JOB/index.lua
+++ handlers/JOB/index.lua
@@ -9,6 +9,7 @@ return (function()
 		NIN = gFunc.LoadFile('smart.lac/handlers/JOB/NIN.lua'),
 		BRD = gFunc.LoadFile('smart.lac/handlers/JOB/BRD.lua'),
 		PLD = gFunc.LoadFile('smart.lac/handlers/JOB/PLD.lua'),
-		WHM = gFunc.LoadFile('smart.lac/handlers/JOB/WHM.lua')
+		WHM = gFunc.LoadFile('smart.lac/handlers/JOB/WHM.lua'),
+		RUN = gFunc.LoadFile('smart.lac/handlers/JOB/RUN.lua')
 	}
 end)()
