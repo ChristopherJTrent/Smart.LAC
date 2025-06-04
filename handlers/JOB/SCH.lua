08c1d21ff9831bdd0801ae12a1cc323c476e8f28
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-06-03 18:17:51 -0700

    add "wrong arts" handler for SCH

--- handlers/JOB/SCH.lua
+++ handlers/JOB/SCH.lua
@@ -1,4 +1,13 @@
 return {
+	precast = function(action, sets)
+		local currentArts = (gData.GetBuffCount("Light Arts") == 1 or gData.GetBuffCount("Addendum: White") == 1) and "White Magic" or "Black Magic"
+		if action.Type ~= currentArts then
+			if sets.precast and sets.precast.wrongarts then
+				return sets.precast.wrongarts
+			end
+		end
+		return false
+	end,
 	midcast = function(action, sets)
 		if sets.midcast == nil then return false end
 		---@type Action
