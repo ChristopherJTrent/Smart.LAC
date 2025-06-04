8846944ddeda85a9996f5a0e987183701749d872
ChristopherJTrent <ChristopherJTrent@outlook.com>
2025-06-03 17:59:46 -0700

    add odyssey gear to profile types

---
+++ definitions/profiletypes.h.lua
@@ -0,0 +1,91 @@
+---@meta
+
+---@class Agwu
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field Axe string
+---@field Greatsword string
+---@field Scythe string
+
+---@class Bunzi
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field Club string
+---@field Great_Axe string
+
+---@class Gleti
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field Crossbow string
+---@field Dagger string
+---@field Great_Katana string
+
+---@class Ikenga
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field Axe string
+---@field Polearm string
+---@field Katana string
+
+---@class Mpaca
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field Bow string
+---@field Staff string
+
+---@class Sakpata
+---@field Head string
+---@field Body string
+---@field Hands string
+---@field Legs string
+---@field Feet string
+---@field HandToHand string
+---@field Sword string
+
+---@class Odyssey
+---@field Agwu Agwu
+---@field Bunzi Bunzi
+---@field Gleti Gleti
+---@field Ikenga Ikenga
+---@field Mpaca Mpaca
+---@field Sakpata Sakpata
+
+---@class JSE_AR
+---@field Head string|table
+---@field Body string|table
+---@field Hands string|table
+---@field Legs string|table
+---@field Feet string|table
+
+---@class JSE_E
+---@field Head string|table
+---@field Body string|table
+---@field Hands string|table
+---@field Legs string|table
+---@field Feet string|table
+---@field Earring string|table
+
+---@class JSE_C
+---@field Adoulin { [string]: string|table }
+---@field Ambuscade { [string]: string|table }
+
+---@class JSE
+---@field Artifact JSE_AR
+---@field Relic JSE_AR
+---@field Empyrean JSE_E
+---@field Capes JSE_C
