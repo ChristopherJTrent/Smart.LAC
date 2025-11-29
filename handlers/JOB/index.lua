697eda6a54b19e801ce5616d9a94e19cfd6d9208
ChristopherJTrent <christopherjtrent@outlook.com>
2025-11-28 22:55:33 -0800

    merge index.lua

--- handlers/JOB/index.lua
+++ handlers/JOB/index.lua
@@ -1,7 +1,6 @@
 ---@alias jobHandlers table<string, jobHandler>
 ---@type jobHandlers
-return (function()
-	return T{
+return T{
 		COR = gFunc.LoadFile('smart.lac/handlers/JOB/COR.lua'),
 		GEO = gFunc.LoadFile('smart.lac/handlers/JOB/GEO.lua'),
 		SCH = gFunc.LoadFile('smart.lac/handlers/JOB/SCH.lua'),
