72d45e664c1196fa6b404f03cce5079b85cbbf50
ChristopherJTrent <christopherjtrent@outlook.com>
2025-11-30 18:00:37 -0800

    bump version

--- smart.lua
+++ smart.lua
@@ -1,5 +1,5 @@
 --luacheck: globals gFunc gData gSettings gProfile chat T
-Smart_Version = "0.6.9"
+Smart_Version = "0.6.10"
 ---@type skills?
 local skills = gFunc.LoadFile('smart.lac/data/skills.lua')
 ---@type playerData?
