8a319715113aefde65e8e05ffca7fc9234152edd
ChristopherJTrent <christopherjtrent@outlook.com>
2025-11-30 17:59:40 -0800

    update DNC handler

--- handlers/JOB/DNC.lua
+++ handlers/JOB/DNC.lua
@@ -6,12 +6,15 @@ return {
         if sets.ability.jigs and string.find(action.Name, " Jig") then
             return gFunc.Combine(sets.ability.default or {}, sets.ability.jigs)
         end
-        if sets.ability.steps and string.find(action.Name, " Step") then
+        if sets.ability.steps and string.find(action.Name, "[sS]tep") then
             return gFunc.Combine(sets.ability.default or {}, sets.ability.steps)
         end
         if sets.ability.sambas and string.find(action.Name, " Samba") then
             return gFunc.Combine(sets.ability.default or {}, sets.ability.sambas)
         end
+		if sets.ability.waltzes and string.find(action.Name, 'Waltz') and action.Name ~= 'Healing Waltz' then
+			return gFunc.Combine(sets.ability.default or {}, sets.ability.waltzes)
+		end
         return false
     end
 }
