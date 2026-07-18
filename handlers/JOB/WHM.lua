9c716a2f51af010f06c1e5aa65269e1453398dbe
ChristopherJTrent <christopherjtrent@outlook.com>
2026-07-17 21:13:23 -0700

    update WHM job handler

--- handlers/JOB/WHM.lua
+++ handlers/JOB/WHM.lua
@@ -3,13 +3,27 @@ return T{
 	midcast = function(action, sets)
 		-- Thanks to Arieh for these lists.
 		local barSpells = T{
+			'Barfira', -- this looks weird, but this data change makes the code much easier to write.
+			'Barblizzara',
+			'Baraera',
+			'Barstonra',
+			'Barthundra',
+			'Barwatera',
+			'Baramnesra',
+			'Barvira',
+			'Barparalyzra',
+			'Barsilencera',
+			'Barpetra',
+			'Barpoisonra',
+			'Barblindra',
+			'Barsleepra',
 			['element'] = T{'Barfira', 'Barblizzara', 'Baraera', 'Barstonra', 'Barthundra', 'Barwatera'},
 			['status'] = T{'Baramnesra', 'Barvira', 'Barparalyzra', 'Barsilencera', 'Barpetra', 'Barpoisonra', 'Barblindra', 'Barsleepra'},
 		}
 		local naSpells = T{'Blindna', 'Esuna', 'Paralyna', 'Poisona', 'Silena', 'Stona', 'Viruna'}
 		if sets.midcast then
 			local finalSet = sets.midcast.default or {}
-			if sets.midcast.barSpell and barSpells:any(function(v) v:contains(action.Name) end) then
+			if sets.midcast.barSpell and barSpells:contains(action.Name) then
 				finalSet = gFunc.Combine(finalSet, sets.midcast.barSpell)
 			end
 			if sets.midcast.barElement and barSpells.element:contains(action.Name) then
